159f3d4d46c07f9c4dd2e3e66a74545efc3995c9
Alex Insayt <mr.insayt@gmail.com>
2021-10-22 00:35:18 +0300

    vue install

--- src/mainLib.js
+++ src/mainLib.js
@@ -1,4 +1,9 @@
 import BIcon from "./components/b-icon";
 import BBtn from "./components/b-btn";
 
-export { BIcon, BBtn };
+export default {
+  install(Vue) {
+    Vue.component("b-icon", BIcon);
+    Vue.component("b-btn", BBtn);
+  },
+};
